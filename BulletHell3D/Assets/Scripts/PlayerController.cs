9986b42332ad7890abda0222bcaa9467bd88697f
DESKTOP-4VTG6G7\spart <evansmtyler@gmail.com>
2018-03-29 14:47:51 -0400

    Added simple player movement
    Added a player model with PlayerController script. Can move the player in the Main scene using wasd and direction keys, or by using the analog sticks(the intended use)

---
+++ BulletHell3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal"); // get player left jostick/directional key input
+        float verticalMove = Input.GetAxis("Vertical"); 
+        Rigidbody playerRigidbody = GetComponent<Rigidbody>(); // get rigibody physics comp
+        Vector3 newVel = new Vector3(horizontalMove, 0.0f, verticalMove); 
+        playerRigidbody.velocity = newVel * speed * Time.fixedDeltaTime; // set new velocity to be the one from input * speed value scaled by deltatime
+        Vector3 newDir = new Vector3(Input.GetAxis("Rhorizontal"), 0.0f, Input.GetAxis("Rvertical"));
+        playerRigidbody.rotation = Quaternion.LookRotation(newDir * Time.fixedDeltaTime);      // set players orientation == direction of second analog stick/axis
+    }
+
+}
