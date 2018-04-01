93cf7ff12297a3250938ef5b032cef83848619c4
DESKTOP-4VTG6G7\spart <evansmtyler@gmail.com>
2018-03-31 20:14:20 -0400

    Added Bullet Cooldown
    Added Bullet cooldown and lifetime.

--- BulletHell3D/Assets/Scripts/Bullet.cs
+++ BulletHell3D/Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     public float speed;
+    public int bulletLife;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +13,11 @@ public class Bullet : MonoBehaviour {
     private void FixedUpdate()
     {
         transform.position += transform.forward * speed * Time.fixedDeltaTime;
+        if (bulletLife <= 0)
+        {
+            Destroy(gameObject);
+        }
+        bulletLife--;
        
     }
 }
--- BulletHell3D/Assets/Scripts/PlayerController.cs
+++ BulletHell3D/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,32 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public int Health;
     public GameObject bullet;
+    public float bulletCooldown;
+    private float oldTime;
 
+
+
+    private void Start()
+    {
+        oldTime = bulletCooldown;
+    }
     private void FixedUpdate()
     {
+        
         Rigidbody playerRigidbody = GetComponent<Rigidbody>(); // get rigibody physics comp
         float horizontalMove = Input.GetAxis("Horizontal"); // get player left jostick/directional key input
         float verticalMove = Input.GetAxis("Vertical"); 
         Vector3 newVel = new Vector3(horizontalMove, 0.0f, verticalMove); 
         playerRigidbody.velocity = newVel * speed * Time.fixedDeltaTime; // set new velocity to be the one from input * speed value scaled by deltatime
         Vector3 newDir = new Vector3(Input.GetAxis("Rhorizontal"), 0.0f, Input.GetAxis("Rvertical"));
-        playerRigidbody.rotation = Quaternion.LookRotation(newDir * Time.fixedDeltaTime);      // set players orientation == direction of second analog stick/axis8
-
+        playerRigidbody.rotation = Quaternion.LookRotation(newDir * Time.fixedDeltaTime);      // set players orientation == direction of second analog stick/axis
         float attack = Input.GetAxis("PrimaryAttack");
         Vector3 newpos = transform.position + transform.forward * 2;
-        if (attack != 0)
+        bulletCooldown -= Time.fixedDeltaTime;
+        if (attack != 0 && bulletCooldown <= 0)
         {
             Instantiate(bullet, newpos, Quaternion.LookRotation(transform.forward));
+            bulletCooldown = oldTime;
         }
     }
 
