6d795c7b50846052d252fe15de0e78a8e1daf687
Amalie Kettering <amaliemkettering@gmail.com>
2018-04-03 16:27:05 -0400

    Will Work on Weapons, Bullets, and PowerUps later this evening
    Probably

---
+++ BulletHell3D/Assets/Scripts/WeaponClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponClass : MonoBehaviour {
+
+    public Bullet current_Bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
