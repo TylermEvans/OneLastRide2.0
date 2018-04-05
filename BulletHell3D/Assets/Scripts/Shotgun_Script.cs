91e7f09e846525467dd655b4b06fb15a708938fc
Amalie Kettering <amaliemkettering@gmail.com>
2018-04-05 14:14:52 -0400

    minor bug
    there i fixed it

--- BulletHell3D/Assets/Scripts/Shotgun_Script.cs
+++ BulletHell3D/Assets/Scripts/Shotgun_Script.cs
@@ -16,10 +16,4 @@ public class Shotgun_Script : WeaponBaseClass {
     {
         // refer to pattern and do something ?
     }
-
-    public void print_stats()
-    {
-        Console.WriteLine("damage: {0}\n speed: {1}", damage, speed);
-        Console.WriteLine(weapon_name);
-    }
 }
